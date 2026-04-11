c2aa9774fab3b5c973ea6fe12ae409165be6d359
Leon <leonlong2004@outlook.com>
2026-04-11 17:11:25 -0400

    Add robot_core package with initial code - implemented simple cpp publisher Twist to a /test_cmd_vel topic, which could be picked up by the robot_bringup subscriber

---
+++ ros2_ws/src/robot_core/src/publisher.cpp
@@ -0,0 +1,39 @@
+#include <chrono>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+using namespace std::chrono_literals;
+
+class Publisher : public rclcpp::Node
+{
+public:
+    Publisher()
+    : Node("publisher"), count_(0)
+    {
+        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/test_cmd_vel", 10);
+        auto timer_callback = [this]() -> void {
+            auto message = geometry_msgs::msg::Twist();
+            message.linear.x = 0.5;
+            message.angular.z = 0.5;
+            RCLCPP_INFO(this->get_logger(), "Publishing: '%f, %f'", message.linear.x, message.angular.z);
+            publisher_->publish(message);
+        };
+        timer_ = this->create_wall_timer(500ms, timer_callback);
+    }
+
+private:
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Publisher>());
+    rclcpp::shutdown();
+    return 0;
+}
